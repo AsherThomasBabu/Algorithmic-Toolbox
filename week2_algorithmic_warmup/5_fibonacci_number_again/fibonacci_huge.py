23beb6f753be7c5c93ab9ef981204f707529173e
Asher Thomas Babu <ashertb2000@gmail.com>
2020-05-03 15:11:11 +0530

    Completed 5-Week_2

--- week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,19 +1,26 @@
 # Uses python3
 import sys
 
-def get_fibonacci_huge_naive(n, m):
+def get_fibonacci_huge_naive(n,m):
     if n <= 1:
         return n
-
-    previous = 0
-    current  = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-
-    return current % m
+    p =[0,1]
+    pisano =0
+    while True:
+        p.append((p[-1] + p[-2])%m)
+        if p[-2:]==[0,1]:
+            pisano = len(p)-2
+            p =[0,1]
+            break
+    if ((n%pisano)-1) < 0:
+        return 0
+    for _ in range ((n%pisano)-1):
+        
+        p.append(p[-1] + p[-2])
+        p.pop(-3)
+    return p[-1]%m
 
 if __name__ == '__main__':
-    input = sys.stdin.read();
+    input = sys.stdin.read()
     n, m = map(int, input.split())
     print(get_fibonacci_huge_naive(n, m))
